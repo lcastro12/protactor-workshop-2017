1dfb80825e5db54741edb861de3b25bc9b240433
Laura Castro Cuellar <lcastroc@pslcol.com.co>
2018-07-25 10:15:25 -0500

    análisis código estático

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -2,18 +2,18 @@ import { browser, Config } from 'protractor';
 import { reporter }   from './helpers/reporter';
 
 export const config: Config = {
- framework: 'jasmine',
- SELENIUM_PROMISE_MANAGER: false,
- specs: ['../test/Google.spec.js'],
- noGlobals: true,
- onPrepare: () => {
-   browser.ignoreSynchronization = true;
-   reporter();
- },
- capabilities: {
+  framework: 'jasmine',
+  SELENIUM_PROMISE_MANAGER: false,
+  specs: ['../test/Google.spec.js'],
+  noGlobals: true,
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+    reporter();
+  },
+  capabilities: {
     browserName: 'chrome',
     chromeOptions: {
-      args: ['--headless', '--disable-gpu', '--window-size=800,600','--no-sandbox']
+      args: ['--headless', '--disable-gpu', '--window-size=800,600', '--no-sandbox']
     }
   }
-}
+};
