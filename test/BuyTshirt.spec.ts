1e601e79d7e43c7d1f9a004e0e999ff5a79d7d50
Laura Castro Cuellar <lcastroc@pslcol.com.co>
2018-07-31 10:38:51 -0500

    use before(), fix use 'it'

--- test/BuyTshirt.spec.ts
+++ test/BuyTshirt.spec.ts
@@ -3,61 +3,63 @@ import { MenuContentPage, ProductAddedModalPage, ProductDetailPage,
    SummaryStepPage, SignInStepPage, AddressStepPage, ShippingStepPage,
    PaymentStepPage, BankPaymentPage, OrderResumePage } from '../src/page';
 
-describe('Abrir la página en el navegador', () => {
-  it('entonces debería abrirse la página', async () => {
-    await browser.get('http://automationpractice.com/');
-
-  });
-
-  describe('Proceso de compra de la camiseta', () => {
-    const menuContentPage: MenuContentPage = new MenuContentPage();
-    const productDetailPage: ProductDetailPage = new ProductDetailPage();
-    const productAddedModalPage: ProductAddedModalPage = new ProductAddedModalPage();
-    const summaryStepPage: SummaryStepPage = new SummaryStepPage();
-    it('entonces se debería comprar la camiseta', async () => {
-
-      await menuContentPage.goToTShirtMenu();
-      await productDetailPage.goToProductDetail();
-      await productAddedModalPage.goToProductAddedModal();
-
-      const expectedCondition = protractor.ExpectedConditions;
-      const condition = expectedCondition
-            .elementToBeClickable(summaryStepPage.getSummaryStepPage);
-      await browser.wait(condition, 3000, 'error waiting');
-      await summaryStepPage.goToSummaryStep();
-
+describe('Buy a t-shirt', () => {
+  describe('Abrir la página en el navegador', () => {
+    beforeAll(async () => {
+      await browser.get('http://automationpractice.com/');
     });
 
-    describe('Logeo en la aplicación', () => {
-      const signInStepPage: SignInStepPage = new SignInStepPage();
-      const addressStepPage: AddressStepPage = new AddressStepPage();
-      it('entonces debería estar logeado en la aplicación', async () => {
+    describe('Proceso de compra de la camiseta', () => {
+      const menuContentPage: MenuContentPage = new MenuContentPage();
+      const productDetailPage: ProductDetailPage = new ProductDetailPage();
+      const productAddedModalPage: ProductAddedModalPage = new ProductAddedModalPage();
+      const summaryStepPage: SummaryStepPage = new SummaryStepPage();
+      beforeAll(async () => {
 
-        await signInStepPage.goToSignInStep();
-        await signInStepPage.addDataSignIn();
-        await addressStepPage.goToAddressStep();
+        await menuContentPage.goToTShirtMenu();
+        await productDetailPage.goToProductDetail();
+        await productAddedModalPage.goToProductAddedModal();
+
+        const expectedCondition = protractor.ExpectedConditions;
+        const condition = expectedCondition
+            .elementToBeClickable(summaryStepPage.getSummaryStepPage);
+        await browser.wait(condition, 3000, 'error waiting');
+        await summaryStepPage.goToSummaryStep();
 
       });
 
-      describe('Seleccionar la dirección por defecto', () => {
-        const shippingStepPage: ShippingStepPage = new ShippingStepPage();
-        it('entonces debería estar seleccionada la dirección', async () => {
+      describe('Logeo en la aplicación', () => {
+        const signInStepPage: SignInStepPage = new SignInStepPage();
+        const addressStepPage: AddressStepPage = new AddressStepPage();
+        beforeAll(async () => {
 
-          await shippingStepPage.goToShippingStep();
-          await shippingStepPage.checkBoxShipping();
+          await signInStepPage.goToSignInStep();
+          await signInStepPage.addDataSignIn();
+          await addressStepPage.goToAddressStep();
 
         });
 
-        describe('Pago en el banco', () => {
-          const paymentStepPage: PaymentStepPage = new PaymentStepPage();
-          const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
-          const orderResumePage: OrderResumePage = new OrderResumePage();
-          it('entonces debería realizar el pago', async () => {
+        describe('Seleccionar la dirección por defecto', () => {
+          const shippingStepPage: ShippingStepPage = new ShippingStepPage();
+          beforeAll(async () => {
 
-            await paymentStepPage.goToPaymentStep();
-            await bankPaymentPage.goToBankPayment();
-            await orderResumePage.goToOrderResume();
-            await orderResumePage.checkText();
+            await shippingStepPage.goToShippingStep();
+            await shippingStepPage.checkBoxShipping();
+
+          });
+
+          describe('Pago en el banco', () => {
+            const paymentStepPage: PaymentStepPage = new PaymentStepPage();
+            const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
+            const orderResumePage: OrderResumePage = new OrderResumePage();
+            it('then should be bought a t-shirt', async () => {
+
+              await paymentStepPage.goToPaymentStep();
+              await bankPaymentPage.goToBankPayment();
+              await orderResumePage.goToOrderResume();
+              await expect(orderResumePage.inicialText.getText())
+            .toBe('Your order on My Store is complete.');
+            });
           });
         });
       });
