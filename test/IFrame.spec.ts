f01710efa14b66c221ad838d70057eebaa74272e
Laura Castro Cuellar <lcastroc@pslcol.com.co>
2018-08-06 11:24:15 -0500

    use of given, when, then

--- test/IFrame.spec.ts
+++ test/IFrame.spec.ts
@@ -1,39 +1,47 @@
 import { browser } from 'protractor';
 import { IFramePage } from '../src/page';
 
-describe('Buy a t-shirt', () => {
-  describe('Abrir la página en el navegador', () => {
+describe('Given the web page with the iFrames', () => {
+  describe('when the web page is open', () => {
     beforeAll(async () => {
       await browser.get('http://toolsqa.com/iframe-practice-page/');
     });
-    describe('Cambiar altura IFrame', () => {
-        const iFramePage: IFramePage= new IFramePage();
+    describe('and i want to change the IFrame height', () => {
+      describe('and verify the page´s title', () => {
+        const iFramePage: IFramePage = new IFramePage();
         beforeAll(async () => {
-            await iFramePage.modifyHeight("600", "IF1");
+          await iFramePage.modifyHeight('600', 'IF1');
         });
-        it('La altura debió ser modificada', async () => {
-            await expect(iFramePage.getHeight()).toBe("600");
+        it('then the IFrame height should have been changed', async () => {
+          await expect(iFramePage.getHeight()).toBe('600');
         });
-        it('Verifica el título principal', async () => {
+        it('and the page´s title is Sample Iframe page', async () => {
           await expect(iFramePage.getTitlePage.getText()).toBe('Sample Iframe page');
         });
 
-        describe('Cambiar de iFrame', () => {
+        describe('when i want to explore the IFrame', () => {
           beforeAll(async () => {
             await iFramePage.switchToFrame1();
           });
-          it('Verificado el título principal del iFrame', async () => {
-            await expect(iFramePage.getTitlePage.getText()).toBe('Practice Automation Form');
-          });
-          describe('Volver al contexto principal', () => {
-            beforeAll(async () => {
-              await iFramePage.switchToInitialContext();
+          describe('and i want to verify the iframe´s title', () => {
+
+            it('then the IFrame title should be Practice Automation Form', async () => {
+              await expect(iFramePage.getTitlePage.getText()).toBe('Practice Automation Form');
             });
-            it('Verificado el título del contexto principal al volver', async () => {
-              await expect(iFramePage.getTitlePage.getText()).toBe('Sample Iframe page');
+            describe('when i want to go back to the initial context', () => {
+              beforeAll(async () => {
+                await iFramePage.switchToInitialContext();
+              });
+              describe('and i want to check the initial page title', () => {
+
+                it('then the title of the initial context should be Sample Iframe page', async () => {
+                  await expect(iFramePage.getTitlePage.getText()).toBe('Sample Iframe page');
+                });
+              });
             });
           });
         });
+      });
     });
   });
 });
