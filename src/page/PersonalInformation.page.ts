450f21a0ed474a6fbd8bd9479dc423643329da74
Laura Castro <l.castro12@uniandes.edu.co>
2018-08-06 08:31:50 -0500

    Descargando archivos (#23)
    * Trabajando con IFrames
    
    * change methods names
    
    * Subiendo un archivo
    
    * Descargando archivos

--- src/page/PersonalInformation.page.ts
+++ src/page/PersonalInformation.page.ts
@@ -1,4 +1,7 @@
 import { ElementFinder, element, by } from 'protractor';
+import { DownloadService } from '../service/Download.service';
+
+const downloadService: DownloadService = new DownloadService();
 
 export class PersonalInformationPage {
 
@@ -42,6 +45,35 @@ export class PersonalInformationPage {
     return element(by.cssContainingText('button', 'Button'));
   }
 
+  public get inputChooseFile(): ElementFinder {
+    return element(by.id('photo'));
+  }
+
+  private async submitFile(filePath: string) {
+    const path = require('path');
+    const absolutePath = path.resolve(__dirname, filePath);
+    await this.inputChooseFile.sendKeys(absolutePath);
+  }
+
+  public async clickButton() {
+    await this.button.click();
+  }
+
+  private get downloadLink(): ElementFinder {
+    return element(by.linkText('Test File to Download'));
+  }
+
+  private async download(fileName: string) {
+
+    const link = await this.downloadLink.getAttribute('href');
+    return await downloadService.downloadFile(link, fileName);
+
+  }
+
+  public checkDownload(fileName: string) {
+    return downloadService.readFileFromTemp(fileName);
+  }
+
   public async fillForm(formData: any): Promise<void> {
     await this.inputForFirstName.sendKeys(formData.firstName);
     await this.inputForLastName.sendKeys(formData.lastName);
@@ -65,6 +97,9 @@ export class PersonalInformationPage {
       await this.getCommand(command).click();
     }
 
-    await this.button.click();
+    if (formData.file) {
+      await this.submitFile(formData.file); }
+
+    await this.download(formData.downloadFileName);
   }
 }
--- test/Locators.spec.ts
+++ test/Locators.spec.ts
@@ -1,13 +1,20 @@
 import { browser, protractor } from 'protractor';
 import { PersonalInformationPage } from '../src/page';
+import { DownloadService } from '../src/service/Download.service';
 
 describe(' Llenar Formulario', () => {
+  const fileName = 'fileDownloaded.xlsx';
+
   describe('Abrir página en el navegador', () => {
+
     beforeAll(async () => {
       await browser.get('http://toolsqa.com/automation-practice-form/');
     });
+
     const personalInformationPage: PersonalInformationPage = new PersonalInformationPage();
+    const downloadService: DownloadService = new DownloadService();
     describe('Ingresar datos', () => {
+
       beforeAll(async () => {
         const expectedCondition = protractor.ExpectedConditions;
         const condition = expectedCondition
@@ -26,12 +33,29 @@ describe(' Llenar Formulario', () => {
             'Navigation Commands',
             'Switch Commands',
             'Wait Commands',
-            'WebElement Commands']
+            'WebElement Commands'],
+          file: '../../../resources/fondo-de-escritorio.jpg',
+          downloadFiles: true,
+          downloadFileName: fileName
         });
       });
-      it('el formulario debió ser completado', async () => {
-        await expect(personalInformationPage.getFormHeader.getText())
-              .toBe('Practice Automation Form');
+      it('la imágen debe estar cargada', async () => {
+        await expect(personalInformationPage.inputChooseFile.getAttribute('value')).toBe('C:\\fakepath\\fondo-de-escritorio.jpg');
+      });
+
+      it('Se debió descargar el archivo', async () => {
+        await expect(downloadService.readFileFromTemp(fileName).byteLength).toBeGreaterThan(0);
+      });
+
+      describe(' seleccionar botón Button luego de llenar datos', () => {
+
+        beforeAll(async () => {
+          await personalInformationPage.clickButton();
+        });
+        it('el formulario debió ser completado', async () => {
+          await expect(personalInformationPage.getFormHeader.getText())
+                .toBe('Practice Automation Form');
+        });
       });
     });
   });
