59c8f3bbe9b1339439237728716197cc2b89ebc2
Laura Castro Cuellar <lcastroc@pslcol.com.co>
2018-08-06 11:30:14 -0500

    lint

--- src/page/IFrame.page.ts
+++ src/page/IFrame.page.ts
@@ -2,29 +2,31 @@
 import { browser, element, by, ElementFinder } from 'protractor';
 
 export class IFramePage {
- 
-    public async modifyHeight(height:string, id: string)  {
-      return await browser.executeScript(`document.getElementById("${id}").setAttribute("height","${height}")`);    
-    }
 
-    private get getiFrame1(): ElementFinder{
-        return element(by.id('IF1'));
-    }
+  public async modifyHeight(height:string, id: string)  {
+    return await
+       browser.executeScript(`document.getElementById("${id}")
+        .setAttribute("height","${height}")`);
+  }
 
-    public async getHeight(){
-        return await this.getiFrame1.getAttribute('height');
-    }
+  private get getiFrame1(): ElementFinder {
+    return element(by.id('IF1'));
+  }
 
-    public get getTitlePage(): ElementFinder {
-        return element(by.id('content')).element(by.tagName('h1'));
-    }
+  public async getHeight() {
+    return await this.getiFrame1.getAttribute('height');
+  }
 
-    public async switchToFrame1() {
-        await browser.switchTo().frame(this.getiFrame1.getWebElement());
-    }
+  public get getTitlePage(): ElementFinder {
+    return element(by.id('content')).element(by.tagName('h1'));
+  }
 
-    public async switchToInitialContext() {
-        await browser.switchTo().defaultContent();
-    }
+  public async switchToFrame1() {
+    await browser.switchTo().frame(this.getiFrame1.getWebElement());
+  }
+
+  public async switchToInitialContext() {
+    await browser.switchTo().defaultContent();
+  }
 
 }
